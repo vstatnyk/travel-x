00d43291907be013c5f5fa291e37d459e75f4d33
vstatnyk <62860849+vstatnyk@users.noreply.github.com>
2022-09-15 13:29:31 -0700

    initial commit

---
+++ data_for_backend/newData.py
@@ -0,0 +1,38 @@
+import json
+from random import randint
+import json
+
+people = {}
+
+with open ("firstNames.txt", 'r') as fName:
+    global firstN
+    firstN = fName.readlines()
+with open ("lastNames.txt", 'r') as lName:
+    global lastN
+    lastN = lName.readlines()
+
+for i in range(0,1000):
+    name = f'{(firstN[randint(0,1947)])[:-1]} {lastN[randint(0,88798)][:-1]}'
+    ssn = f'{randint(0,999)}-{randint(0,99)}-{randint(0,9999)}'
+    passportNumber = f'{randint(0,999999999)}'
+    passportExp = f'{randint(1,12)}/{randint(1,29)}/{randint(2000,2030)}'
+    dob = f'{randint(1,12)}/{randint(1,29)}/{randint(1900,2000)}'
+    dlNumber = f'{randint(0,999999999)}'
+
+    while ssn in people:
+        ssn = f'{randint(0,999)}-{randint(0,99)}-{randint(0,9999)}'
+        while passportNumber in people['ssn']:
+            passportNumber = f'{randint(0,999999999)}'
+        while dlNumber in people['dlNumber']:
+            dlNumber = f'{randint(0,999999999)}'
+    people[ssn] = {"DMV":{"name":name,"dlNumber":dlNumber,"dob":dob,},
+    "SS":{"name":name,"dob":dob},
+    "DOS":{"name":name,"dob":dob,"passportNumber":passportNumber,"passportExp":passportExp}}
+
+with open("rawData.json", "w") as outfile:
+    json.dump(people, outfile, indent =2)
+
+
+print(people)
+
+
