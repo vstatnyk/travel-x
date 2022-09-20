b116029151c554a742564c8e378ef623869d5a6b
vstatnyk <62860849+vstatnyk@users.noreply.github.com>
2022-09-19 17:40:05 -0700

    initial commit

---
+++ travel-x/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+    <button>GO</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ travel-x/src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Button } from 'react-native'
+
+export const Button = () => {
+  return (
+    <Button
+        
+    
+    />
+  )
+}
