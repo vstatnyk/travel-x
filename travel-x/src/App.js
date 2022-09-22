1a9bec9fdd93bc872e275dde376016249ef64341
BrianBeilby <brianbeilby98@gmail.com>
2022-09-21 20:27:29 -0700

    added background

--- travel-x/src/App.js
+++ travel-x/src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
 import EntryBox from "./components/EntryBox";
+import background from "./components/background.png";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <Header title='Welcome to Travel X'/>
       <EntryBox text = "Enter SSN Here: "/>
     </div>
