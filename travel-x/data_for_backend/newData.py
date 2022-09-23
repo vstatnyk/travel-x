5fb30cb07f323138a979fbb8800f259d0102b510
vstatnyk <62860849+vstatnyk@users.noreply.github.com>
2022-09-22 21:04:24 -0700

    new data pt2

--- travel-x/data_for_backend/newData.py
+++ travel-x/data_for_backend/newData.py
@@ -13,18 +13,18 @@ with open ("lastNames.txt", 'r') as lName:
 
 for i in range(0,500):
     name = f'{(firstN[randint(0,19947)])[:-1]} {lastN[randint(0,88798)][:-1]}'
-    ssn = f'{randint(0,999)}-{randint(0,99)}-{randint(0,9999)}'
-    passportNumber = f'{randint(0,999999999)}'
-    passportExp = f'{randint(1,12)}/{randint(1,29)}/{randint(2000,2030)}'
-    dob = f'{randint(1,12)}/{randint(1,29)}/{randint(1900,2000)}'
-    dlNumber = f'{randint(0,999999999)}'
+    ssn = f'{str(randint(0,999)).zfill(3)}-{str(randint(0,99)).zfill(2)}-{str(randint(0,9999)).zfill(4)}'
+    passportNumber = f'{str(randint(0,999999999)).zfill(9)}'
+    passportExp = f'{str(randint(1,12)).zfill(2)}/{str(randint(1,29)).zfill(2)}/{str(randint(2000,2030)).zfill(4)}'
+    dob = f'{str(randint(1,12)).zfill(2)}/{str(randint(1,29)).zfill(2)}/{str(randint(1900,2000)).zfill(4)}'
+    dlNumber = f'{str(randint(0,999999999)).zfill(9)}'
 
     while ssn in people:
-        ssn = f'{randint(0,999)}-{randint(0,99)}-{randint(0,9999)}'
+        ssn = f'{str(randint(0,999)).zfill(3)}-{str(randint(0,99)).zfill(2)}-{str(randint(0,9999).zfill(4))}'
         while passportNumber in people['ssn']:
-            passportNumber = f'{randint(0,999999999)}'
+            passportNumber = f'{str(randint(0,999999999)).zfill(9)}'
         while dlNumber in people['dlNumber']:
-            dlNumber = f'{randint(0,999999999)}'
+            dlNumber = f'{str(randint(0,999999999)).zfill(9)}'
     people[ssn] = {"DMV":{"name":name,"dlNumber":dlNumber,"dob":dob,},
     "SS":{"name":name,"dob":dob},
     "DOS":{"name":name,"dob":dob,"passportNumber":passportNumber,"passportExp":passportExp}}
