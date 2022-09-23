2ec68798588a5bc2fff28f240424ad277516289d
BrianBeilby <brianbeilby98@gmail.com>
2022-09-22 21:00:00 -0700

    updated format of object in .add()

--- travel-x/lib/server.js
+++ travel-x/lib/server.js
@@ -4,34 +4,35 @@ dotenv.config();
 
 // Instantiate the Vendia client and authenticate to it.
 const client = createVendiaClient({
-  apiUrl: `https://515wv89hij.execute-api.us-west-1.amazonaws.com/graphql/`,
-  websocketUrl: `wss://ce3d2k3vfc.execute-api.us-west-1.amazonaws.com/graphql`,
+  apiUrl: `https://cihcal58f3.execute-api.us-west-1.amazonaws.com/graphql/`,
+  websocketUrl: `wss://9ol9kpkz06.execute-api.us-west-1.amazonaws.com/graphql`,
   apiKey: process.env.VENDIA_API_KEY
 });
 
 const { entities } = client;  // entities const to access schema properties.
 
-// Add new entry into DMV array.
-const response1 = await entities.DMV.add({
-  dob: '03/02/2001',
-  name: 'Jared',
-  dlNumber: 'Y89HG651'
-});
-
-// Add new entry into SS array.
-const response2 = await entities.SS.add({
-  dob: '11/22/1999',
-  name: 'Kyle',
-});
-
-// Add new entry into DOS array.
-const response3 = await entities.DOS.add({
-    dob: '10/20/1979',
-    name: 'Alex',
-    passportNumber: '840832461',
-    passportExp: '09/30/2026'
-});
+// Add new entry into People array.
+const response1 = await entities.people.add(
+  {
+    "DMV": {
+      dob: '03/02/2001',
+      name: 'Jared',
+      dlNumber: 'Y89HG651'
+    },
+    "SS": {
+      name: 'Jared',
+      dob: '03/02/2001',
+      ssNum: '876-01-7824'
+    },
+    "DOS": {
+      name: 'Jared',
+      dob: '03/02/2001',
+      passportNumber: '164834623',
+      passportExp: '10/08/2024'
+    }
+  }
+);
 
 // List the entries in DMV array.
-const response4 = await entities.DMV.list();
+const response4 = await entities.people.list();
 console.log(response4);
