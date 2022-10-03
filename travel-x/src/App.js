36f65073e3e9a5bfdc72b68f21a36a189d6a6827
Akemble <113637319+AkembleYuumi1@users.noreply.github.com>
2022-10-03 10:36:33 -0700

    Made image into background with css, tinkered with some other css stuff
    put the image into index.css rather than in the picture, added css reset.

--- travel-x/src/App.js
+++ travel-x/src/App.js
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 import EntryBox from "./components/EntryBox";
 import background from "./components/background.png";
+import React from 'react';
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: `url(${background})` }}>
-      
-      <Header title='Welcome to Travel X'/>
-      <img src = {background} alt = 'background'/>
+    <div className="App">
+      <Header title='Welcome to Travel X' />
       <EntryBox text = "Enter SSN Here: "/>
+      
     </div>
   );
 }
