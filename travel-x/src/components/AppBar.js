63f61b0f8d339ca49d5122b70ef663bf830d5d80
BrianBeilby <brianbeilby98@gmail.com>
2022-10-25 21:06:29 -0700

    changed HOME to TRAVELX

--- travel-x/src/components/AppBar.js
+++ travel-x/src/components/AppBar.js
@@ -57,7 +57,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            HOME
+            TRAVELX
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
