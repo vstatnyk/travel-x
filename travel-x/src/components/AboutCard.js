2213d04ec2441daeedfcbbeb541f3bfc3071deee
Akemble <113637319+AkembleYuumi1@users.noreply.github.com>
2022-11-15 06:48:34 -0800

    About cards now component, made it better

---
+++ travel-x/src/components/AboutCard.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import IconButton, {IconButtonProps } from '@mui/material/IconButton';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Collapse from "@material-ui/core/Collapse";
+import { styled } from '@mui/material/styles';
+import CardActions from '@mui/material/CardActions';
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+}),
+}));
+
+const AboutCard = (props) => {
+    const [expanded, setExpanded] = React.useState(false);
+    
+    const handleExpandClick = i => {
+      setExpanded(!expanded)
+    }; 
+
+    return (
+        <Card sx={{maxWidth: 400}}>
+            <CardMedia
+            component="img"
+            height="400"
+            image={props.imgPath}
+            alt="place holder"
+            />
+            <CardActions disableSpacing>
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        {props.name}
+                    </Typography>
+                </CardContent>
+                <ExpandMore
+                    expand = {expanded}
+                    onClick = {handleExpandClick}
+                    aria-expanded = {expanded}
+                    aria-label = "show more"
+                >
+                    <ExpandMoreIcon/>
+                </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout = "auto" unmountOnExit>
+                <CardContent>
+                    <Typography paragraph>
+                        {props.aboutInfo}
+                    </Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
+    )
+}
+
+export default AboutCard
--- travel-x/src/pages/About.js
+++ travel-x/src/pages/About.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import {Grid, Typography } from '@mui/material';
 import img1 from ".././components/images/Portrait_Placeholder.png";
 import brianImg from "../components/images/brianbeilbypic.jpg";
 import coltonImg from "../components/images/colton_carvalho.jpg";
 import moshImg from "../components/images/Resized Photo for Profile Picture.jpg";
-import vsImg from "../components/images/Vstat.jpg"; 
+import vsImg from "../components/images/Vstat.jpg";
+import AboutCard from '../components/AboutCard';
+
 const About = () => {
   return (
     <Container maxWidth="xxl">
@@ -28,144 +30,56 @@ const About = () => {
       </Box>
       <Box 
         sx={{
-          maxWidth: 4/5,
-          paddingLeft: "18%"
+          flexGrow: 1
         }}
       >
         <Grid 
           container 
           spacing={3}
-          alignItems="center"
-          justifyContent="center"
+          alignItems="stretch"
+          justifyContent="space-evenly"
         >
           <Grid item xs="auto">
-            <Card sx={{maxWidth: 400}}>
-              <CardMedia
-                component="img"
-                height="400"
-                image={brianImg}
-                alt="place holder"
-              />
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Brian Beilby
-                </Typography>
-                <Typography variant="body2">
-                  I am attending CSUS, studying Computer Science. This is my first big web-development project but,
-                  in the past, I have worked on many projects for an internship organization which is where most of my practical
-                  experience has come from. You can contact me directly at
-                  <a href={`mailto:brianbeilby98@gmail.com`}> brianbeilby98@gmail.com</a>.
-                </Typography>
-              </CardContent>
-            </Card>
+            <AboutCard 
+              imgPath={brianImg}
+              name={"Brian Beilby"}
+              aboutInfo={"I am attending CSUS, studying Computer Science. This is my first big web-development project but, in the past, I have worked on many projects for an internship organization which is where most of my practical experience has come from. You can contact me directly at brianbeilby98@gmail.com."}
+            />
           </Grid>
           <Grid item xs="auto">
-          <Card sx={{maxWidth: 400}}>
-              <CardMedia
-                component="img"
-                height="400"
-                image={moshImg}
-                alt="place holder"
-              />
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Moshley Marcelo
-                </Typography>
-                <Typography variant="body2">
-                  I am a CSU Sacramento student majoring in Computer Science. So far most of my
-                  experience has come from recent coursework I have completed in the past. One project
-                  involved the use of UNIX, I made a decision making game based off of
-                  the Oregon Trail game with the use of signed/unsigned jump statements, 
-                  random number statements, & math calculations. Another project, involved a game 
-                  screen with collision detection, 2d movement, character frames, & interaction with 
-                  items. This project so far, is the first i've ever done utilizing JavaScript
-                  , I have worked on the back end of this project mostly adding photos to
-                   on the webpage & Vendia, getting the buttons on the website to work properly,
-                   & getting the webpage to not glitch out & function normally.   
-                    I do intend on doing more within my major, including joining an internship
-                  , joining any clubs, learning more languages including Python, & keeping
-                    my coding skills up to date.
-                  Feel free to contact me @ <a href={`mailto:moshleymarcelo11@yahoo.com`}> moshleymarcelo11@yahoo.com</a>.
-                </Typography>
-              </CardContent>
-            </Card>
+            <AboutCard 
+              imgPath={moshImg}
+              name={"Moshley Marcelo"}
+              aboutInfo={"I am a CSU Sacramento student majoring in Computer Science. So far most of my experience has come from recent coursework I have completed in the past. One project involved the use of UNIX, I made a decision making game based off of the Oregon Trail game with the use of signed/unsigned jump statements, random number statements, & math calculations. Another project, involved a game screen with collision detection, 2d movement, character frames, & interaction with items. This project so far, is the first i've ever done utilizing JavaScript, I have worked on the back end of this project mostly adding photos to on the webpage & Vendia, getting the buttons on the website to work properly, & getting the webpage to not glitch out & function normally. I do intend on doing more within my major, including joining an internship, joining any clubs, learning more languages including Python, & keeping my coding skills up to date. Feel free to contact me @ moshleymarcelo11@yahoo.com."}
+            />
           </Grid>
           <Grid item xs="auto">
-            <Card sx={{maxWidth: 400}}>
-              <CardMedia
-                component="img"
-                height="400"
-                image={coltonImg}
-                alt="place holder"
-              />
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Colton Carvalho
-                </Typography>
-                <Typography variant="body2">
-                  Currently I'm studying for a bachelor's degree in Computer Science at California State University, Sacramento.
-                  On this web project I worked on the backend code for data processing, editing the front end code on an incidental basis.
-                  Most of my experience in working on projects comes from: previous internship opportunities, such as working 
-                  on cognitive and high frequency long range techonology in a 5G environment; freelance client-programmer requests; along with 
-                  personal projects stemming upwards of 8 years back. <br></br> 
-                  Contact me directly at <a href={`mailto:Coolguy1079@gmail.com`}>Coolguy1079@gmail.com</a>.
-                </Typography>
-              </CardContent>
-            </Card>
+            <AboutCard 
+              imgPath={coltonImg}
+              name={"Colton Carvalho"}
+              aboutInfo={"Currently I'm studying for a bachelor's degree in Computer Science at California State University, Sacramento. On this web project I worked on the backend code for data processing, editing the front end code on an incidental basis. Most of my experience in working on projects comes from: previous internship opportunities, such as working on cognitive and high frequency long range techonology in a 5G environment; freelance client-programmer requests; along with personal projects stemming upwards of 8 years back. Contact me directly at Coolguy1079@gmail.com."}
+            />
           </Grid>
           <Grid item xs="auto">
-            <Card sx={{maxWidth: 400}}>
-              <CardMedia
-                component="img"
-                height="400"
-                image={img1}
-                alt="place holder"
-              />
-              <CardContent>
-                <Typography variant="h5" component="div">
-                Test 4
-                </Typography>
-                <Typography variant="body2">
-                  This clown did some coding. What a nerd
-                </Typography>
-              </CardContent>
-            </Card>
+            <AboutCard 
+              imgPath={img1}
+              name={"Test 4"}
+              aboutInfo={"This clown did some coding. What a nerd"}
+            />
           </Grid>
           <Grid item xs="auto">
-            <Card sx={{maxWidth: 400}}>
-              <CardMedia
-                component="img"
-                height="400"
-                image={img1}
-                alt="place holder"
-              />
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Test 5
-                </Typography>
-                <Typography variant="body2">
-                  This clown did some coding. What a nerd
-                </Typography>
-              </CardContent>
-            </Card>
+            <AboutCard 
+              imgPath={img1}
+              name={"Test 5"}
+              aboutInfo={"This clown did some coding. What a nerd"}
+            />
           </Grid>
           <Grid item xs="auto">
-            <Card sx={{maxWidth: 400}}>
-              <CardMedia
-                component="img"
-                height="400"
-                image={vsImg}
-                alt="place holder"
-              />
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  Vlad Statnyk
-                </Typography>
-                <Typography variant="body2">
-                  This clown did some coding. What a nerd
-                </Typography>
-              </CardContent>
-            </Card>
+            <AboutCard 
+              imgPath={vsImg}
+              name={"Vlad Statnyk"}
+              aboutInfo={"This clown did some coding. What a nerd"}
+            />
           </Grid>
         </Grid>
       </Box>
